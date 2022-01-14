0e7a108fa60a9f9ad3ae3e8d5b9cf70861c0faad
Ethan Manis <74456094+ethan3212@users.noreply.github.com>
2022-01-14 14:37:21 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+
+namespace Admission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double GPA;
+            int testScore;
+            const double minGPA = 3.0;
+            const int admin1 = 60;
+            const int admin2 = 80;
+
+            string inputString;
+            Console.WriteLine("Enter grade point average: ");
+            inputString = Console.ReadLine();
+            GPA = Convert.ToDouble(inputString);
+
+            Console.WriteLine("Enter test score: ");
+            inputString = Console.ReadLine();
+            testScore = Convert.ToInt32(inputString);
+
+            if (GPA >= minGPA)
+                if (testScore >= admin1)
+                    Console.WriteLine("Accept ");
+                else
+                    Console.WriteLine("Reject ");
+            else
+                if (testScore >= admin2)
+                Console.WriteLine("Accept ");
+            else
+                Console.WriteLine("Reject ");
+        }
+    }
+}
